b1a4bc6cd6bac849657ec6f29bcd5980af1d32bc
nin-jin <sairi-na-tenshi@ya.ru>
2023-06-01 16:40:31 +0000

    Deploying to gh-pages from @ hyoo-ru/lupa.hyoo.ru@c3b71dac964b920324b0bcb31b4c08fd360f1e25 🚀

--- node.mjs
+++ node.mjs
@@ -3380,8 +3380,11 @@ var $;
     var $$;
     (function ($$) {
         $mol_style_define($hyoo_lupa, {
+            overflow: 'hidden',
             Camera: {
                 objectFit: 'cover',
+                transform: 'scale(2)',
+                filter: 'contrast(1.25)',
             },
         });
     })($$ = $.$$ || ($.$$ = {}));
