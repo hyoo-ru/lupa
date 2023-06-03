262be835d28016ef23727e477ca6573ee7a1bc12
nin-jin <sairi-na-tenshi@ya.ru>
2023-06-03 11:33:12 +0000

    Deploying to gh-pages from @ hyoo-ru/lupa.hyoo.ru@8f408216eb55ef697dd29801baa07e1ca8fccdd2 🚀

--- node.mjs
+++ node.mjs
@@ -4746,6 +4746,7 @@ var $;
             const obj = new this.$.$mol_video_camera();
             obj.title = () => "$hyoo_lupa - PWA Magnifying Glass";
             obj.facing = () => "environment";
+            obj.size = () => null;
             obj.sharpness = () => 4;
             obj.torch = () => true;
             return obj;
