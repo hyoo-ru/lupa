24aaf333efd6ac5d6223d3bcae6d84e01e67e374
nin-jin <nin-jin@users.noreply.github.com>
2023-07-18 14:56:23 +0000

    Deploying to gh-pages from @ hyoo-ru/lupa.hyoo.ru@de29bbb80839e7255573dcbda43138f879926915 🚀

--- node.test.js
+++ node.test.js
@@ -839,8 +839,8 @@ var $;
         }
         static send(handler, params = {}) {
             const glob = $mol_dom_context;
-            glob.androidBridge?.[handler](JSON.stringify(params));
-            glob.iosBridge?.[handler].postMessage(params);
+            glob.AndroidBridge?.[handler](JSON.stringify(params));
+            glob.webkit?.messageHandlers?.[handler].postMessage(params);
             glob.ReactNativeWebView?.postMessage({ handler, params });
             glob.parent.postMessage({ handler, params, type: 'vk-connect' }, '*');
         }
