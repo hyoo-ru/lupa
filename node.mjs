af1c37a6b526726b76cff33fa0e02e486c7254cb
nin-jin <nin-jin@users.noreply.github.com>
2023-10-10 07:11:14 +0000

    Deploying to gh-pages from @ hyoo-ru/lupa.hyoo.ru@a383171e74c245a04d84f5b9dde75b904b4d1fd9 🚀

--- node.mjs
+++ node.mjs
@@ -3481,6 +3481,15 @@ var $;
         dom_name() {
             return "a";
         }
+        uri_off() {
+            return "";
+        }
+        uri_native() {
+            return null;
+        }
+        external() {
+            return false;
+        }
         attr() {
             return {
                 ...super.attr(),
