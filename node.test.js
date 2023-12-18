4edfb00d9011aee976de1483b39838e950f0bdff
nin-jin <nin-jin@users.noreply.github.com>
2023-12-18 07:11:41 +0000

    Deploying to gh-pages from @ hyoo-ru/lupa.hyoo.ru@a383171e74c245a04d84f5b9dde75b904b4d1fd9 🚀

--- node.test.js
+++ node.test.js
@@ -3530,7 +3530,8 @@ var $;
                 title: this.hint_safe(),
                 target: this.target(),
                 download: this.file_name(),
-                mol_link_current: this.current()
+                mol_link_current: this.current(),
+                rel: this.relation()
             };
         }
         sub() {
@@ -3564,6 +3565,9 @@ var $;
         }
         current() {
             return false;
+        }
+        relation() {
+            return "";
         }
         event_click(event) {
             if (event !== undefined)
--- web.js
+++ web.js
@@ -3081,7 +3081,8 @@ var $;
                 title: this.hint_safe(),
                 target: this.target(),
                 download: this.file_name(),
-                mol_link_current: this.current()
+                mol_link_current: this.current(),
+                rel: this.relation()
             };
         }
         sub() {
@@ -3115,6 +3116,9 @@ var $;
         }
         current() {
             return false;
+        }
+        relation() {
+            return "";
         }
         event_click(event) {
             if (event !== undefined)
